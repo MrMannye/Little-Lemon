fc7a8547a7a0e4d1bc65e12b191e80653d6aa3da
Miguel Aguilera <superbrandon2018@gmail.com>
2023-07-04 01:29:26 -0500

    ⭐ Alert reservation

---
+++ src/components/Modals/Alert.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Alert({name}) {
+  return (
+    <div className='alert'>
+        <h1>Estimado {name} su reservacion ha sido exitosa</h1>
+    </div>
+  )
+}
